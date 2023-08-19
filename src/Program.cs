d0c8e2eca723ef7c00b883ff594db38cf4873970
terminite1 <terminitetp@gmail.com>
2023-08-19 15:25:43 +0200

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using System.Net;
+using System.Text.Json;
+using System.Diagnostics;
 
 namespace ExclusiveFullscreen;
 
@@ -12,16 +14,25 @@ public static class Program
     }
     static void Main()
     {
+        if (Debugger.IsAttached)
+        {
+            logc("Debug mode enabled. Press any key to continue.");
+            Console.ReadKey();
+            logc("\n");
+            Debug();
+        }
         logc("Roblox ClientAppSettings.json setup by | termizzle (@terminite)");
-
         var client = new HttpClient();
-        var response = client.GetAsync("http://setup.roblox.com/version").Result;
+        // issue 1 fix:::::
+        var response = client.GetAsync("https://clientsettingscdn.roblox.com/v1/client-version/WindowsPlayer").Result;
         var content = response.Content.ReadAsStringAsync().Result;
+        var jsons = JsonDocument.Parse(content);
+        content = jsons.RootElement.GetProperty("clientVersionUpload").GetString();
         logc("Current Roblox version: "+content+"\n");
-
+        
         if (Directory.Exists(@"C:\Program Files (x86)\Roblox\"))
         {
-            logc("Found Roblox in Program Files (x86). Please reinstall Roblox without Administrator permissions.");
+            logc("Found Roblox in Program Files (x86). Please reinstall Roblox without Administrator permissions.\nOr just delete the folder if you're not using that one.");
             Console.ReadKey();
             Environment.Exit(0);
         }
@@ -34,16 +45,112 @@ public static class Program
             if (!Directory.Exists(roblox_dir + @"\ClientSettings"))
             {
                 Directory.CreateDirectory(roblox_dir + @"\ClientSettings");
-                logc("Created clientsettings folder");
+                logc("Created ClientSettings folder");
             }
 
-            using (var client2 = new WebClient())
+            var clientObj = new
             {
-                client2.DownloadFile("https://cdn.discordapp.com/attachments/786964016546709514/1132097819969343558/ClientAppSettings.json", roblox_dir + @"\ClientSettings\ClientAppSettings.json");
-                logc("Downloaded ClientAppSettings.json");
+                FFlagDebugGraphicsPreferD3D11 = "True",
+                FFlagFixGraphicsQuality = "True",
+                FFlagHandleAltEnterFullscreenManually = "False"
+            };
+            string jsonString = JsonSerializer.Serialize(clientObj);
+            logc("Would you like to use the default ClientAppSettings.json or the RCO one?");
+            logc("1. Default");
+            logc("2. RCO");
+            string? choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                logc("Using default ClientAppSettings.json");
+                File.WriteAllText(roblox_dir + @"\ClientSettings\ClientAppSettings.json", jsonString);
+            }
+            else if (choice == "2")
+            {
+                logc("Using RCO ClientAppSettings.json");
+                logc("Downloading ClientAppSettings.json");
+                using (WebClient client2 = new WebClient())
+                {
+                    client2.DownloadFile("https://raw.githubusercontent.com/L8X/Roblox-Client-Optimizer/main/ClientAppSettings.json", roblox_dir + @"\ClientSettings\ClientAppSettings.json");
+                }
+            }
+            else
+            {
+                logc("Invalid choice. You should blow up. By the way, press any key...");
+                Console.ReadKey();
+                Environment.Exit(0);
             }
         }
-        logc("\nOk so, we done bruh. Press any key to exit.");
+        logc("\nAll done!!!!! Press any key to exit.");
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
+    public static void Debug()
+    {
+        logc("[DEBUG] Roblox ClientAppSettings.json setup by | termizzle (@terminite)");
+        var client = new HttpClient();
+        var response = client.GetAsync("https://clientsettingscdn.roblox.com/v1/client-version/WindowsPlayer").Result;
+        logc("Sending GET to https://clientsettingscdn.roblox.com/v1/client-version/WindowsPlayer");
+        var content = response.Content.ReadAsStringAsync().Result;
+        logc("Response: " + content);
+        var jsons = JsonDocument.Parse(content);
+        logc("Parsing JSON");
+        content = jsons.RootElement.GetProperty("clientVersionUpload").GetString();
+        logc("Extracted Version");
+        logc("Current Roblox version: " + content + "\n");
+
+        if (Directory.Exists(@"C:\Program Files (x86)\Roblox\"))
+        {
+            logc("Found Roblox in Program Files (x86). Please reinstall Roblox without Administrator permissions. Or just delete the folder if you're not using that one.");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        if (Directory.Exists(@"C:\Users\" + Environment.UserName + @"\AppData\Local\Roblox\"))
+        {
+            string roblox_dir = @"C:\Users\" + Environment.UserName + @"\AppData\Local\Roblox\Versions\" + content;
+            logc("Found Roblox Version || " + roblox_dir);
+
+            if (!Directory.Exists(roblox_dir + @"\ClientSettings"))
+            {
+                Directory.CreateDirectory(roblox_dir + @"\ClientSettings");
+                logc("Created ClientSettings folder");
+            }
+
+            var clientObj = new
+            {
+                FFlagDebugGraphicsPreferD3D11 = "True",
+                FFlagFixGraphicsQuality = "True",
+                FFlagHandleAltEnterFullscreenManually = "False"
+            };
+            string jsonString = JsonSerializer.Serialize(clientObj);
+            logc("Would you like to use the default ClientAppSettings.json or the RCO one?");
+            logc("1. Default");
+            logc("2. RCO");
+            string? choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                logc("Using default ClientAppSettings.json");
+                File.WriteAllText(roblox_dir + @"\ClientSettings\ClientAppSettings.json", jsonString);
+            }
+            else if (choice == "2")
+            {
+                logc("Using RCO ClientAppSettings.json");
+                logc("Downloading ClientAppSettings.json");
+                using (WebClient client2 = new WebClient())
+                {
+                    client2.DownloadFile("https://raw.githubusercontent.com/L8X/Roblox-Client-Optimizer/main/ClientAppSettings.json", roblox_dir + @"\ClientSettings\ClientAppSettings.json");
+                }
+            }
+            else
+            {
+                logc("Invalid choice. You should blow up. By the way, press any key...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+        logc("\nAll done!!!!! Press any key to exit.");
         Console.ReadKey();
         Environment.Exit(0);
     }
